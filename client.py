a3d2869d17cdb140e60c7d46d8fb4e09b95ae651
shruti280401 <75423179+shruti280401@users.noreply.github.com>
2021-06-19 14:34:21 +0530

    client.py

--- client.py
+++ client.py
@@ -1,15 +1,3 @@
-'''import socket
-s=socket.socket()
-host=socket.gethostname()
-port=12345
-#s.connect(('192.168.0.107',12345))
-s.connect((host,port))
-while True:
-
-    print(s.recv(1024))
-    #s.close()'''
-
-
 
 import socket
 s=socket.socket()
