98849d493ee8bb801ef1b17c45b85a1dc90c412d
shruti280401 <75423179+shruti280401@users.noreply.github.com>
2021-06-19 14:32:48 +0530

    server.py

--- server.py
+++ server.py
@@ -1,23 +1,3 @@
-'''import socket
-s=socket.socket()
-host=socket.gethostname()
-print(host)
-port=12345
-s.bind((host,port))
-s.listen(5)
-while True:
-
-    c, addr = s.accept()
-    while True:
-        n = input("enter n:")
-        print('connection', addr)
-        c.send(bytes(n, encoding='utf8'))
-    # for i in range(10):
-    #
-    # c.close()'''
-
-
-
 
 import socket
 s=socket.socket()
@@ -40,7 +20,7 @@ while True:
 
 
 
-            #while True:
+            
 
 
 
